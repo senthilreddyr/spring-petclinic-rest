a8d8f70319c57fd0d9520f5fd240f3f42edc8382
senthilreddy <r.senthilreddy@gmail.com>
2021-04-12 13:10:48 +0530

    updated inallers.sh

--- installers/install.sh
+++ installers/install.sh
@@ -5,4 +5,4 @@ sudo apt -y install openjdk-8-jre openjdk-8-jdk
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
 wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get -y install postgresql
+sudo apt-get -y install postgresql-client
